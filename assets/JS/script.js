785e14712d425c0240d87b887e4c021ecce31baf
dukanegolder <dukanegolder@gmail.com>
2022-12-27 12:59:36 -0700

    added functions to the JS

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,30 +1,37 @@
-'use strict'
-
-
 var generateBtn = document.querySelector("#generate");
-var uppercase = `A B C D E F G H I J K L M N O P Q R S T U V W X Y Z`
-var lowercase = uppercase.toLowerCase()
-var symboles = `! @ # $ % ^ & * ( ) - + ? > < ~`
-var numberschar = `1 2 3 4 5 6 7 8 9`
+var uppercaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var lowercaseChsr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var symboleChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 function generatePassword() {
-  console.log("Generating password")
 
-
+  var option = [];
   var lengthOfPassword = prompt("How many characters would you like?")
+  if (lengthOfPassword < 8 || lengthOfPassword > 128) {
+    alert('You have not selected the right amount of charachters!!')
+    return
+  }
 
   var wantsUpper = confirm("Do you want uppercase letters?")
-  console.log(wantsUpper)
 
   var wantLower = confirm("Do you want lowercase letters?")
-  console.log(wantLower)
 
-  if ((lengthOfPassword) === "") {
-    alert("You didn't select any data types")
-    return
-  } else if (lengthOfPassword, wantsUpper, wantLower) {
+  var wantSymbole = confirm('Do you want symboles?')
 
+  if (wantsUpper === true) {
+    option = option.concat(uppercaseChar)
   }
+
+  if (wantLower === true) {
+    option = option.concat(lowercaseChsr)
+  }
+
+  if (wantSymbole === true) {
+    option = option.concat(symboleChar)
+  }
+
+  return
 }
 
 
@@ -41,3 +48,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+console.log(Math.floor(Math.random() * uppercaseChar.length))
